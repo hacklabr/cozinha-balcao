b6d7674f6cbe76ede1506a68684054cbd4d95a12
Rafael Monico <monico@macminico>
2014-10-02 18:05:36 -0300

    Mostrando o nome do projeto ativo no painel

--- painel/views.py
+++ painel/views.py
@@ -1,10 +1,23 @@
+# coding: utf-8
+
 from django.views.generic import ListView
+
+import requests
 
 class IndexView(ListView):
     template_name = 'painel/index.html'
     context_object_name = 'project_list'
 
     def get_queryset(self):
-        projects = ["Projeto 1", "Projeto 2"]
+        projects = self.getProjectList()
 
         return projects
+
+    def getProjectList(self):
+        key='a0334645b2c8c9da0d96a5fd5d15e629f737818b'
+
+        r = requests.get('http://code-devel.hacklab.com.br/projects.json?key=' + key)
+
+        j = r.json()
+
+        return j['projects']
