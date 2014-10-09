87540c43334aa96cb8a61798e8d6513eb95b44a2
Rafael Monico <rmonico@moningrado>
2014-10-09 02:05:10 -0300

    Adicionado ao modelo classe de sensor/acao e novo status de iniciado

--- painel/admin.py
+++ painel/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from painel.models import ProjetoPessoa
 from painel.models import Projeto
-
+from painel.models import Status
+from painel.models import Acao
+from painel.models import Sensor
 
 admin.site.register(ProjetoPessoa)
 admin.site.register(Projeto)
+admin.site.register(Status)
+admin.site.register(Acao)
+admin.site.register(Sensor)
--- painel/models.py
+++ painel/models.py
@@ -4,20 +4,13 @@ from django.db import models
 from django.conf import settings
 
 class Projeto(models.Model):
-    STATUS=[
-        ['aguardando_inicio', 'Aguardando início']
-        ['desenvolvimento', 'Desenvolvimento']
-        ['staging', 'Staging']
-        ['maturacao', 'Maturação']
-        ['manutencao', 'Manutenção']
-        ['entregue', 'Entregue']
-    ]
     nome=models.CharField(max_length=200)
+    slug=models.CharField(max_length=200)
     cliente_nome=models.CharField(max_length=200)
     equipe=models.ManyToManyField(settings.AUTH_USER_MODEL, through='ProjetoPessoa')
     created=models.DateTimeField(auto_now_add=True)
     last_modified=models.DateTimeField(auto_now=True)
-    status=models.CharField(max_length=64, choices=STATUS)
+    e_modelo=models.BooleanField(default=False)
 
     def __str__(self):
         return unicode(self).encode('utf-8')
@@ -25,6 +18,32 @@ class Projeto(models.Model):
     def __unicode__(self):
         return self.nome
 
+
+class Status(models.Model):
+    STATUS=[
+        ['aguardando_inicio', 'Aguardando início'],
+        ['iniciado', 'Iniciado'],
+        ['desenvolvimento', 'Desenvolvimento'],
+        ['staging', 'Staging'],
+        ['maturacao', 'Maturação'],
+        ['manutencao', 'Manutenção'],
+        ['entregue', 'Entregue'],
+    ]
+    nome=models.CharField(max_length=64, choices=STATUS)
+    projeto=models.ForeignKey(Projeto)
+
+
+class Dispositivo(object):
+    nome=models.CharField(max_length=200)
+    python_command_line=models.CharField(max_length=400)
+    status=models.ForeignKey(Status)
+
+
+class Sensor(Dispositivo, models.Model):
+    pass
+
+class Acao(Dispositivo, models.Model):
+    pass
 
 class ProjetoPessoa(models.Model):
     PAPEIS=[
@@ -43,9 +62,3 @@ class ProjetoPessoa(models.Model):
     def __unicode__(self):
         # TODO Usar o first_name e o last_name, apenas se os dois estiverem vazios/nulos usar o username
         return self.pessoa.username + " em " + self.projeto.nome
-
-
-class Acao(models.Model):
-    nome=models.CharField(max_length=100)
-    python_command_line=models.CharField(max_length=400)
-    projeto=models.ForeignKey(Projeto)
