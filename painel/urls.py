1db9e9db7200f51b353b1b463b56d1b6cfb7caea
Rafael Monico <monico@macminico>
2014-10-09 18:50:39 -0300

    urls.py formated de acordo com o pep8

--- painel/urls.py
+++ painel/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 from painel import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', views.IndexView.as_view(), name='index'),
 )
