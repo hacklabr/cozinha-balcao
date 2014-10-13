beca901550d968117fe99d433155543ae4815437
Rafael Monico <monico@macminico>
2014-10-13 13:59:24 -0300

    Created initial version of script

---
+++ planning/script.mm
@@ -0,0 +1,189 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1411671923747" ID="ID_1590384980" MODIFIED="1413219528455" TEXT="Script">
+<node CREATED="1411671935445" ID="ID_1369325427" MODIFIED="1412705095019" POSITION="right" TEXT="Criar P&#xe1;gina do Projeto">
+<node CREATED="1412275542250" ID="ID_1136203619" MODIFIED="1412275556582" TEXT="Nome"/>
+<node CREATED="1412275556881" ID="ID_537920452" MODIFIED="1412275559590" TEXT="Slug"/>
+<node CREATED="1412275560561" ID="ID_385492179" MODIFIED="1412287660667" TEXT="L&#xed;der"/>
+<node CREATED="1412275591121" ID="ID_1966601794" MODIFIED="1412275592638" TEXT="Equipe"/>
+<node CREATED="1412275563241" ID="ID_1289879677" MODIFIED="1412275565374" TEXT="Cliente"/>
+<node CREATED="1412275565609" ID="ID_760706391" MODIFIED="1412275571342" TEXT="Contatos"/>
+<node CREATED="1412275571553" ID="ID_741757519" MODIFIED="1412275574894" TEXT="Descri&#xe7;&#xe3;o"/>
+<node CREATED="1412275575185" ID="ID_229432812" MODIFIED="1412280480731" TEXT="Per&#xed;odo de desenvolvimento"/>
+<node CREATED="1412276001327" FOLDED="true" ID="ID_1216892868" MODIFIED="1412282091773" TEXT="Per&#xed;odo de manuten&#xe7;&#xe3;o">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Informar quando estiver proximo a data de fim da matura&#231;&#227;o
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1412276055870" ID="ID_1788508888" MODIFIED="1412276058643" TEXT="Avisar o cliente"/>
+<node CREATED="1412276058855" ID="ID_1662133917" MODIFIED="1412276062619" TEXT="Avisar o hacklab"/>
+</node>
+<node CREATED="1412275947535" ID="ID_1571634564" MODIFIED="1412280496947" TEXT="Per&#xed;odo de Matura&#xe7;&#xe3;o">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Informar quando estiver proximo a data de fim da matura&#231;&#227;o
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1412276238230" ID="ID_807973778" MODIFIED="1412280512478" TEXT="Per&#xed;odo de hospedagem"/>
+<node CREATED="1412276231678" ID="ID_1248792393" MODIFIED="1412276235586" TEXT="Hospedagem ativa?"/>
+<node CREATED="1412275607993" ID="ID_1595870773" MODIFIED="1412277612887" TEXT="Status">
+<node CREATED="1412276402676" ID="ID_512255872" MODIFIED="1412280008768" TEXT="Aguardando in&#xed;cio">
+<node CREATED="1412280009905" ID="ID_302564357" MODIFIED="1412280010246" TEXT="Status padr&#xe3;o"/>
+</node>
+<node CREATED="1412276408117" ID="ID_1691124148" MODIFIED="1412280001600" TEXT="Desenvolvimento">
+<node CREATED="1412281583648" ID="ID_316071032" MODIFIED="1412281602596" TEXT="Canal de comunica&#xe7;&#xe3;o deve existir"/>
+<node CREATED="1412280012849" ID="ID_1953864908" MODIFIED="1412280055622" TEXT="Reposit&#xf3;rio do projeto deve existir"/>
+<node CREATED="1412280071753" ID="ID_268092569" MODIFIED="1412281957651" TEXT="Estrutura do WP deve existir no reposit&#xf3;rio"/>
+<node CREATED="1412280111112" ID="ID_438261912" MODIFIED="1412281759312" TEXT="Server de testing deve existir"/>
+<node CREATED="1412280156016" ID="ID_727116439" MODIFIED="1412280168061" TEXT="Server de testing deve possuir uma c&#xf3;pia do projeto"/>
+<node CREATED="1412281790623" ID="ID_211351322" MODIFIED="1412281804676" TEXT="Server de testing deve possuir um webserver rodando"/>
+</node>
+<node CREATED="1412276422420" ID="ID_1537378175" MODIFIED="1412276425695" TEXT="Staging">
+<node CREATED="1412280127208" ID="ID_1331305189" MODIFIED="1412280135005" TEXT="Servidor de produ&#xe7;&#xe3;o deve existir"/>
+<node CREATED="1412280156016" ID="ID_18129804" MODIFIED="1412280296228" TEXT="Server de produ&#xe7;&#xe3;o deve possuir uma c&#xf3;pia do projeto"/>
+<node CREATED="1412281868175" ID="ID_509454931" MODIFIED="1412281873572" TEXT="Server de produ&#xe7;&#xe3;o deve possuir um webserver rodando"/>
+</node>
+<node CREATED="1412276411685" ID="ID_151992466" MODIFIED="1412276416793" TEXT="Matura&#xe7;&#xe3;o">
+<node CREATED="1412280324175" ID="ID_1842178316" MODIFIED="1412280338772" TEXT="Deploy deve ter sido conclu&#xed;do ao menus uma vez com sucesso"/>
+</node>
+<node CREATED="1412276416997" ID="ID_1631696069" MODIFIED="1412276422113" TEXT="Manuten&#xe7;&#xe3;o">
+<node CREATED="1412280350103" ID="ID_876591005" MODIFIED="1412280548891" TEXT="Data atual deve estar dentro do per&#xed;odo de manuten&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1412276428220" ID="ID_912726219" MODIFIED="1412276431865" TEXT="Entregue">
+<node CREATED="1412280350103" ID="ID_1146022333" MODIFIED="1412280566131" TEXT="Data atual deve estar depois do per&#xed;odo de manuten&#xe7;&#xe3;o"/>
+</node>
+</node>
+</node>
+<node CREATED="1411671953183" ID="ID_1643747242" MODIFIED="1411671961708" POSITION="right" TEXT="Criar canal de comunica&#xe7;&#xe3;o"/>
+<node CREATED="1411671973616" ID="ID_1070246665" MODIFIED="1412705089890" POSITION="right" TEXT="Criar Ambiente de Desenvolvimento">
+<node CREATED="1411671982612" ID="ID_717370929" MODIFIED="1411671999834" TEXT="Criar reposit&#xf3;rio">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      No github
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1411672020086" ID="ID_1144488327" MODIFIED="1411672098757" TEXT="Criar branchs">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Testing e Stable
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1411672078001" ID="ID_1429352389" MODIFIED="1411672086266" TEXT="Commitar estrutura WP"/>
+</node>
+<node CREATED="1411672103032" FOLDED="true" ID="ID_791670699" MODIFIED="1412704952789" POSITION="right" TEXT="Configurar server de Testing">
+<node CREATED="1411672128191" ID="ID_1054794177" MODIFIED="1411672129281" TEXT="TODO"/>
+</node>
+<node CREATED="1411672137539" ID="ID_1816749272" MODIFIED="1412281656549" POSITION="right" TEXT="Configurar server de produ&#xe7;&#xe3;o">
+<node CREATED="1411672150104" ID="ID_1651013403" MODIFIED="1411672168854" TEXT="Criar m&#xe1;quina">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Manual por enquanto, sempre no Amazon
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1411672173864" ID="ID_1709482729" MODIFIED="1411672176030" TEXT="Configs"/>
+<node CREATED="1411672176290" ID="ID_83557269" MODIFIED="1411672179000" TEXT="Deploy"/>
+<node CREATED="1411672179240" ID="ID_302146933" MODIFIED="1411672183142" TEXT="Senha Ngnix"/>
+<node CREATED="1411672183433" ID="ID_1530210724" MODIFIED="1411672188169" TEXT="Trocar dom&#xed;nio"/>
+<node CREATED="1411672188411" ID="ID_456699253" MODIFIED="1411672196568" TEXT="Download do Pacote"/>
+<node CREATED="1411672196791" ID="ID_675925679" MODIFIED="1411672201203" TEXT="Monitoramento">
+<node CREATED="1412281390769" ID="ID_1911960948" MODIFIED="1412281393695" TEXT="Espa&#xe7;o em disco"/>
+<node CREATED="1412281393985" ID="ID_1202498061" MODIFIED="1412281397111" TEXT="Tr&#xe1;fego de rede"/>
+<node CREATED="1412276362093" ID="ID_1503994810" MODIFIED="1412282126618" TEXT="Vers&#xe3;o do WP atualizada"/>
+<node CREATED="1412281397529" ID="ID_1709309739" MODIFIED="1412281408887" TEXT="Alertar em caso de anormalidade"/>
+</node>
+<node CREATED="1411672201510" ID="ID_1591270865" MODIFIED="1411672203375" TEXT="Backup"/>
+</node>
+<node CREATED="1411672345006" FOLDED="true" ID="ID_1591656274" MODIFIED="1412704997189" POSITION="right" TEXT="Codifica&#xe7;&#xe3;o">
+<node CREATED="1411672376584" ID="ID_1892833899" MODIFIED="1411672381774" TEXT="Selecionar Issue"/>
+<node CREATED="1411672382067" ID="ID_1189500176" MODIFIED="1411672397491" TEXT="Criar Branch">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Espec&#237;fica para a Issue
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1411672399511" ID="ID_1934272761" MODIFIED="1411672401551" TEXT="Codar"/>
+<node CREATED="1411672401808" ID="ID_1199733012" MODIFIED="1411672406441" TEXT="Fazer merge no master"/>
+</node>
+<node CREATED="1411672428368" ID="ID_419904350" MODIFIED="1411672457993" POSITION="right" TEXT="Sprint conclu&#xed;do?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Se sim, ir para o deploy, se n&#227;o ir para a codifica&#231;&#227;o
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1411678234194" FOLDED="true" ID="ID_711157100" MODIFIED="1412705001621" POSITION="right" TEXT="Deploy">
+<node CREATED="1411672473832" ID="ID_268317913" MODIFIED="1411672484361" TEXT="Criar Tag com n&#xfa;mero da vers&#xe3;o"/>
+<node CREATED="1411672485490" ID="ID_1305956680" MODIFIED="1411672487645" TEXT="Build"/>
+<node CREATED="1411672487905" ID="ID_462414681" MODIFIED="1411672489362" TEXT="Deploy"/>
+</node>
+<node CREATED="1411672524224" ID="ID_17199031" MODIFIED="1411678249996" POSITION="right" TEXT="Backup">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Backup do c&#243;digo, reposit&#243;rio (bare), uploads e dump do DB. J&#225; automatizado por script.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1411672543488" ID="ID_392976460" MODIFIED="1411672566084" POSITION="right" TEXT="Projeto conclu&#xed;do?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Se sim, finalizar
+    </p>
+    <p>
+      Se n&#227;o irpara codifica&#231;&#227;o
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1411672569401" ID="ID_937367634" MODIFIED="1411672574016" POSITION="right" TEXT="Fim"/>
+</node>
+</map>
